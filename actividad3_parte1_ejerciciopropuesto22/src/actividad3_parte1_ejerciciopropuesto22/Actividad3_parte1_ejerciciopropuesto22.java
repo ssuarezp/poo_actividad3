564c0161b9bae71bd08b980390740218ddf3e1fb
ssuarezp <ssuarezp@unal.edu.co>
2024-12-04 21:25:45 -0500

    1

--- actividad3_parte1_ejerciciopropuesto22/src/actividad3_parte1_ejerciciopropuesto22/Actividad3_parte1_ejerciciopropuesto22.java
+++ actividad3_parte1_ejerciciopropuesto22/src/actividad3_parte1_ejerciciopropuesto22/Actividad3_parte1_ejerciciopropuesto22.java
@@ -15,6 +15,8 @@ public class Actividad3_parte1_ejerciciopropuesto22 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
     }
     
 }
---
+++ actividad3_parte1_ejerciciopropuesto22/src/actividad3_parte1_ejerciciopropuesto22/Empleado.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividad3_parte1_ejerciciopropuesto22;
+
+/**
+ *
+ * @author EstudianteAuxiliar
+ */
+public class Empleado {
+    
+    public static double calcular_salario_mensual(double salario_basico_hora, double numero_horas_trabajadas){
+        
+        double salario_mensual;
+        
+        salario_mensual = salario_basico_hora * numero_horas_trabajadas;
+        
+        if (salario_mensual > 450000){
+            return salario_mensual;
+        }
+        else{
+            return salario_mensual;
+        }
+        
+    }
+    
+    public static String set_Nombre(String nombre){
+        
+        return nombre;
+        
+    }
+    
+    public static String limpiar_campos(){
+        return "";
+    }
+
+    
+}
